dd59b93d04f62beed07e55ad2aed31c2b10daac0
Jorge Home <Jhome@intergrupo.com>
2018-11-29 12:34:47 -0500

    first commit

---
+++ my-app/src/components/navigation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            0
+        )
+    }
+}
+
+export default Navigation;
